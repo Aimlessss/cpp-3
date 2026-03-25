96c0d3e883ee931898d6280a2facc8b503d95696
Atharva <katharva2002@gmail.com>
2026-03-25 13:29:48 +0000

    comt

--- lc/cp.cpp
+++ lc/cp.cpp
@@ -1,29 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int arr[n];
-        for(int i=0;i<n;i++){
-            cin>>arr[i];
-        }
+struct TreeNode {
+    int val;
+    TreeNode* right;
+    TreeNode* left;
+    TreeNode(int input){
+        this->val = input;
+        this->right = nullptr;
+        this->left = nullptr;
+    }
+};
+    unordered_map<TreeNode*, TreeNode*> mp;
+    TreeNode* startNode;
+    void buildGraph(TreeNode* root, int start){
+        if(!root) return;
 
-        for(int i = 0; i < n; i++){
-            int count1 = 0;
-            int count2 = 0;
-            for(int j = i + 1; j < n; j++){
-                if(arr[i] > arr[j]){
-                    count1++;
-                }else if(arr[i] < arr[j]){
-                    count2++;
+        if(start == root->val){
+            startNode = root;
+        }
+        if(root->left){
+            mp[root->left] = root;
+        }
+        buildGraph(root->left, start);
+        if(root->right){
+            mp[root->right] = root;
+        }
+        buildGraph(root->right, start);
+    }
+    void bfs(TreeNode* startNode, int time){
+        queue<TreeNode*>q;
+        unordered_set<int>vis;
+        q.push(startNode);
+        vis.insert(startNode->val);
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                TreeNode* curr = q.front(); q.pop();
+                if(curr->left && !vis.count(curr->val)){
+                    q.push(curr->left);
+                }
+                if(curr->right && !vis.count(curr->val)){
+                    q.push(curr->right);
+                }
+                if(mp.count(curr) && !vis.count(mp[curr]->val)){
+                    q.push(mp[curr]);
                 }
             }
-            cout<<max(count1, count2)<<" ";
+            time++;
         }
-        cout<<endl;
     }
+    int amountOfTime(TreeNode* root, int start) {
+        if(!root) return 0;
+        startNode = NULL;
+        buildGraph(root, start);
+        int time = 0;
+        bfs(startNode, time);
+        return time;
+    }
+
+int main(){
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    return 0;
 }
