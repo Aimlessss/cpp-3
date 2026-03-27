8a6587a17487350466025778b717f23f33238fc2
Atharva <katharva2002@gmail.com>
2026-03-27 05:07:11 +0000

    code change daily cp

--- lc/cp.cpp
+++ lc/cp.cpp
@@ -11,56 +11,70 @@ struct TreeNode {
         this->left = nullptr;
     }
 };
-    unordered_map<TreeNode*, TreeNode*> mp;
-    TreeNode* startNode;
-    void buildGraph(TreeNode* root, int start){
-        if(!root) return;
 
-        if(start == root->val){
-            startNode = root;
-        }
+    unordered_map<TreeNode*, TreeNode*>mp;
+    void createGraph(TreeNode* root){
+        if(!root)return;
         if(root->left){
             mp[root->left] = root;
         }
-        buildGraph(root->left, start);
         if(root->right){
             mp[root->right] = root;
         }
-        buildGraph(root->right, start);
+        createGraph(root->left);
+        createGraph(root->right);
     }
-    void bfs(TreeNode* startNode, int time){
-        queue<TreeNode*>q;
-        unordered_set<int>vis;
-        q.push(startNode);
-        vis.insert(startNode->val);
+    TreeNode* findStartValue(TreeNode* root, int startValue){
+        if(!root) return nullptr;
+
+        if(root->val == startValue){
+            return root;
+        }
+
+        TreeNode* leftResult = findStartValue(root->left, startValue);
+        if(leftResult) return leftResult;
+
+        TreeNode* rightResult = findStartValue(root->right, startValue);
+        return rightResult;
+    }
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+        if(!root) return "";
+        createGraph(root);
+        TreeNode* startValueNode = findStartValue(root, startValue);
+        queue<pair<TreeNode* ,string>>q;
+        q.push({startValueNode, ""});
+        unordered_set<TreeNode*>vis;
+        vis.insert(startValueNode);
         while(!q.empty()){
-            int sz = q.size();
-            while(sz--){
-                TreeNode* curr = q.front(); q.pop();
-                if(curr->left && !vis.count(curr->val)){
-                    q.push(curr->left);
+            int n = q.size();
+            while(n--){
+                auto [curr, currPath] = q.front();q.pop();
+                if(curr->val = destValue) return currPath;
+                if(curr->left && !vis.count(curr->left)){
+                    q.push({curr->left, currPath + 'L'});
+                    vis.insert(curr->left);
                 }
-                if(curr->right && !vis.count(curr->val)){
-                    q.push(curr->right);
+                if(curr->right && !vis.count(curr->left)){
+                    q.push({curr->right, currPath + 'L'});
+                    vis.insert(curr->right);
                 }
-                if(mp.count(curr) && !vis.count(mp[curr]->val)){
-                    q.push(mp[curr]);
+                if(mp.find(curr)!= mp.end() && !vis.count(mp[curr])){
+                    q.push({mp[curr], currPath + 'U'});
+                    vis.insert(mp[curr]);
                 }
             }
-            time++;
         }
-    }
-    int amountOfTime(TreeNode* root, int start) {
-        if(!root) return 0;
-        startNode = NULL;
-        buildGraph(root, start);
-        int time = 0;
-        bfs(startNode, time);
-        return time;
+        return "";
     }
 
+
 int main(){
-    TreeNode* root = new TreeNode(1);
-    root->left = new TreeNode(2);
+    TreeNode* root = new TreeNode(5);
+    root->left = new TreeNode(1);
+    root->left->left = new TreeNode(3);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(4);
+    cout<<getDirections(root, 3, 6);
     return 0;
 }
