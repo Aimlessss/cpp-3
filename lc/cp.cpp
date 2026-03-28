6cd0471b8f82184f82772bccd57985f76be03ce3
Atharva <katharva2002@gmail.com>
2026-03-28 03:55:17 +0000

    cp

--- lc/cp.cpp
+++ lc/cp.cpp
@@ -3,78 +3,104 @@ using namespace std;
 
 struct TreeNode {
     int val;
-    TreeNode* right;
     TreeNode* left;
-    TreeNode(int input){
-        this->val = input;
-        this->right = nullptr;
-        this->left = nullptr;
+    TreeNode* right;
+    TreeNode(int x){
+        this->val = x;
+        left = nullptr;
+        right = nullptr;
     }
 };
 
-    unordered_map<TreeNode*, TreeNode*>mp;
-    void createGraph(TreeNode* root){
+    struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+    unordered_map<TreeNode*, TreeNode*>parent;
+    vector<TreeNode* >leafs;
+    bool check(ListNode* head, TreeNode* root){
+        if(!head || !root)return false;
+        if(head->val != root->val)[
+            return false;
+        ]
+        return check(head->next, root->left) || check(head->next, root->right);
+    }
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if(!head || !root)return false;
+
+        return check(head, root) || isSubPath(head, root->left) || isSubPath(head, root->right);
+    }
+    void createGraph(TreeNode* root, ListNode* list){
         if(!root)return;
         if(root->left){
-            mp[root->left] = root;
+            parent[root->left] = root;
         }
         if(root->right){
-            mp[root->right] = root;
+            parent[root->right] = root;
+        }
+
+        if(!root->left && !root->right){
+            leafs.push_back(root);
         }
         createGraph(root->left);
         createGraph(root->right);
     }
-    TreeNode* findStartValue(TreeNode* root, int startValue){
-        if(!root) return nullptr;
-
-        if(root->val == startValue){
-            return root;
-        }
-
-        TreeNode* leftResult = findStartValue(root->left, startValue);
-        if(leftResult) return leftResult;
-
-        TreeNode* rightResult = findStartValue(root->right, startValue);
-        return rightResult;
-    }
-    string getDirections(TreeNode* root, int startValue, int destValue) {
-        if(!root) return "";
-        createGraph(root);
-        TreeNode* startValueNode = findStartValue(root, startValue);
-        queue<pair<TreeNode* ,string>>q;
-        q.push({startValueNode, ""});
+    int dfs(TreeNode* leaf, int distance, unordered_map<TreeNode*, int>& visLeafs){
+        if(!leaf)return 0;
+        int count = 0;
+        queue<pair<TreeNode*, int>>q;
         unordered_set<TreeNode*>vis;
-        vis.insert(startValueNode);
+        int currDist = 0;
+        q.push({leaf, currDist});
+        vis.insert(leaf);
         while(!q.empty()){
-            int n = q.size();
-            while(n--){
-                auto [curr, currPath] = q.front();q.pop();
-                if(curr->val = destValue) return currPath;
-                if(curr->left && !vis.count(curr->left)){
-                    q.push({curr->left, currPath + 'L'});
-                    vis.insert(curr->left);
-                }
-                if(curr->right && !vis.count(curr->left)){
-                    q.push({curr->right, currPath + 'L'});
-                    vis.insert(curr->right);
-                }
-                if(mp.find(curr)!= mp.end() && !vis.count(mp[curr])){
-                    q.push({mp[curr], currPath + 'U'});
-                    vis.insert(mp[curr]);
+            auto [currLeaf, currDist] = q.front();q.pop();
+            if(currDist > distance) continue;
+            if(!currLeaf->left && !currLeaf->right){
+                if(visLeafs.find(currLeaf) == visLeafs.end()){
+                    count++;
                 }
             }
+            if(currLeaf->left && !vis.count(currLeaf->left)){
+                q.push({currLeaf->left, currDist + 1});
+                vis.insert(currLeaf->left);
+            }
+            if(currLeaf->right && !vis.count(currLeaf->right)){
+                q.push({currLeaf->right, currDist + 1});
+                vis.insert(currLeaf->left);
+            }
+            if(parent[currLeaf] && !vis.count(parent[currLeaf])){
+                q.push({parent[currLeaf], currDist + 1});
+                vis.insert(parent[currLeaf]);
+            }
         }
-        return "";
+        return count;
+    }
+    int countPairs(TreeNode* root, int distance) {
+        if(!root)return 0;
+        createGraph(root);
+        int n = leafs.size();
+        unordered_map<TreeNode*, int>visLeaf;
+        int pairs = 0;
+        for(int i = 0; i < leafs.size(); i++){
+            TreeNode* currLeaf = leafs[i];
+            visLeaf[currLeaf] = 1;
+            pairs+=dfs(currLeaf, distance, visLeaf);
+        }
+        return pairs;
     }
 
 
 int main(){
-    TreeNode* root = new TreeNode(5);
-    root->left = new TreeNode(1);
-    root->left->left = new TreeNode(3);
-    root->right = new TreeNode(2);
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right = new TreeNode(3);
     root->right->left = new TreeNode(6);
-    root->right->right = new TreeNode(4);
-    cout<<getDirections(root, 3, 6);
-    return 0;
+    root->right->right = new TreeNode(7);
+     cout<<countPairs(root, 3);
 }
