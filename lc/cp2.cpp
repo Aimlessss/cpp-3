7a85ce9abc934fb11fc55a717479708c14812964
Atharva <katharva2002@gmail.com>
2026-04-01 17:52:57 +0000

    another code

--- lc/cp2.cpp
+++ lc/cp2.cpp
@@ -1,54 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-        map<int,int>unqiueMap;
-        map<int, multiset<int>, greater<int>> eventPriorityMap;
-class EventManager {
-public:
-        // map<int,int>unqiueMap;
-        // map<int, multiset<int>, greater<int>> eventPriorityMap;
-    EventManager(vector<vector<int>>& events) {
-        for(int i = 0; i < events.size(); i++){
-            int eventId = events[i][0];
-            int priority = events[i][1];
-            unqiueMap[eventId] = priority;
-            eventPriorityMap[priority].insert(eventId);
-        }
 
-    }
-    
-    void updatePriority(int eventId, int newPriority) {
-        int currPriority = unqiueMap[eventId];
-        multiset<int>set = eventPriorityMap[currPriority];
-        auto it = set.find(eventId);
-        if(it != set.end()){
-            set.erase(it);
-        }
-        eventPriorityMap[currPriority] = set;
-        unqiueMap[eventId] = newPriority;
-        multiset<int>newPrioritySet = eventPriorityMap[newPriority];
-        newPrioritySet.insert(eventId);
-    }
-    
-    int pollHighest() {
-        auto it = eventPriorityMap.begin();
-
-        int largestKey = it->first;
-        multiset<int>& values = it->second;
-        int ans = -1;
-        if(!values.empty()){
-            ans = *values.begin();
-            unqiueMap.erase(ans);
-            values.erase(ans);
-        }
-        return ans;
-    }
-};
-
-int main(){
-    vector<vector<int>> events = {{5, 7}, {2, 7}, {9, 4}};
-    EventManager em(events);
-    cout<<em.pollHighest()<<endl;
-    em.updatePriority(9, 7);
-    cout<<em.pollHighest()<<endl;
-    cout<<em.pollHighest()<<endl;
+int main() {
+	// your code goes here
+	int n, q;
+	cin>>n>>q;
+	int arr[n];
+	for(int i = 0; i < n; i++){
+	    cin>>arr[i];
+	}
+	sort(arr, arr + n);
+	while(q--){
+	    int x;
+	    cin>>x;
+	    int latest = 0;
+	    int ans = 0;
+	    for(int i = 0; i < n; i++){
+	        if (i > 0 && arr[i] == arr[i-1]) continue;
+	        if(latest < arr[i]){
+	            ans+=x;
+	        }else{
+	            int temp = arr[i] + x;
+	            ans+=temp - latest;
+	        }
+	        latest = arr[i] + x;
+	    }
+	}
+	return 0;
 }
