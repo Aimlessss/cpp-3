6239b8439e8fc82e34a678d26134770a59b1c10d
Atharva <katharva2002@gmail.com>
2026-05-18 07:17:36 +0000

    commit

--- lc/cp.cpp
+++ lc/cp.cpp
@@ -1,29 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	// your code goes here
-	int t;
-	cin>>t;
-	while(t--){
-	    int n;
-        cin>>n;
-        vector<int> a(n);
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-        }
-        int ans = 0;
-        for(int i = 0; i < 31; i++){
-            int countBits = 0;
-            for(int j = 0; j < n; j++){
-                int bit = (a[j] >> i) & 1;
-                countBits += bit;
-            }
-            if(countBits % 2 == 1){
-                ans |= (1 << i);
-            }
-        }
-        cout<<ans<<endl;
-	}
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+        queue<int>q;
+        vector<int>vis(n, false);
 
+        q.push(start);
+        vis[start] = true;
+
+        while(!q.empty()){
+            int n = q.size();
+            while(n--){
+                int frontIdx = q.front(); q.pop();
+                if(arr[frontIdx] == 0)return true;
+
+                int newIdxPos = frontIdx + arr[frontIdx];
+                int newIdxNeg = arr[frontIdx] - frontIdx;
+
+                if(!(newIdxNeg < 0 || newIdxNeg >= arr.size() || vis[newIdxNeg])){
+                    q.push(newIdxNeg);
+                    vis[newIdxNeg] = true;
+                };
+
+                if(!(newIdxPos < 0 || newIdxPos >= arr.size() || vis[newIdxPos])){
+                    q.push(newIdxPos);
+                    vis[newIdxPos] = true;  
+                };
+            }
+        }
+        return false;
+    }
+};
+
+
+int main() {
+    // vector<int>v = {3, 2, 1, 0, 4};
+
+    //[4,2,3,0,3,1,2], start = 5
+
+    Solution s;
+    vector<int>v = {4,2,3,0,3,1,2};
+    s.canReach(v, 5);
 }
--- lc/cp2.cpp
+++ lc/cp2.cpp
@@ -1,86 +1,30 @@
 #include <bits/stdc++.h>
-using namespace std;
-#define ll long long
 
+using namespace std;
 class Solution {
 public:
-    bool check(int target, vector<vector<int>>& tasks){
-        for(int i = 0; i < tasks.size(); i++){
-            int first = tasks[i][0];
-            int second = tasks[i][1];
+    void solve(vector<vector<int>>& res, vector<int>& nums, vector<int>curr, int start){
+            res.push_back(curr);
 
-            if(target < second)return false;
-            target-=first;
+        for(int i = start; i < nums.size(); i++){
+            curr.push_back(nums[i]);
+            solve(res, nums, curr, start + 1);
+            curr.pop_back();
+            solve(res, nums, curr, start + 1);
         }
-        return true;
     }
-    int minimumEffort(vector<vector<int>>& tasks) {
-        int maxSecond = 0;
-        int prefSum = 0;
-        for(auto& x : tasks){
-            prefSum+=x[0];
-            maxSecond = max(maxSecond, x[1]);
-        }
-        sort(tasks.begin(), tasks.end());
-        int left = 1;
-        int right = maxSecond + prefSum;
-        int res = right;
-        while(left <= right){
-            int midd = left + (right - left) / 2;
-
-            if(check(midd, tasks)){
-                res = midd;
-                right = midd - 1;
-            }else{
-                left = midd + 1;
-            }
-        }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>res;
+        vector<int>curr;
+        solve(res, nums, curr, 0);
         return res;
     }
 };
 
 int main() {
-    ll t;
-    cin>>t;
-    while(t--){
-        ll n;
-        cin>>n;
-        ll temp = n;
-        ll arr[n];
-        for(ll i = 0; i < n; i++){
-            cin>>arr[i];
-        }
-        stack<ll>factors;
-        for(ll i = 1; i <= sqrt(n); i++){
-            if(n % i == 0){
-                factors.push(i);
-                if(n / i != i){
-                    factors.push(n / i);
-                }
-            }
-        }
-        ll maxRes = 0;
-        while(!factors.empty()){
-            temp = factors.top();
-            factors.pop();
-            ll i = 0;
-            ll j = 0;
-            ll maxNum = LONG_LONG_MIN;
-            ll minNum = LONG_LONG_MAX;
-            ll currSum = 0;
-            while(j < n){
-                currSum+=arr[j];
-                if(j - i + 1 >= temp){
-                    maxNum = max(maxNum, currSum);
-                    minNum = min(minNum, currSum);
-                    currSum = 0;
-                    i = j + 1;
-                }
-                j++;
-            }
-            maxRes = max(maxRes, abs(maxNum - minNum));
-        }
-        cout<<maxRes<<endl;
-    }
-    return 0;
+    // your code goes here
+
 }
+
+// 2 2 3 1 3 3 1 2
+// 2 2 2 1 3 3 1 2
